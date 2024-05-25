20de65e0c23eba3fe6cfd9d63be23d6376181950
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2024-05-25 16:25:15 -0300

    add suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+"use client";
+
+import { Suspense } from "react";
 import { WebGl } from "./three/canvas";
 
 export default function TrackerPage() {
   return (
     <main className="flex min-h-[100svh] w-full relative">
       <div className="absolute inset-0">
-        <WebGl />
+        <Suspense>
+          <WebGl />
+        </Suspense>
       </div>
     </main>
   );
